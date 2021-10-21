0d7c6bc375100decf74faa21c91d66a293370e47
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2021-10-21 14:58:16 +0200

    update alembic revision
    - use importlib instead of pkg_resources
    - registering in t_modules is handled by geonature directly

--- backend/gn_module_template/migrations/06261234b984_create_template_schema.py
+++ backend/gn_module_template/migrations/06261234b984_create_template_schema.py
@@ -5,11 +5,10 @@ Revises:
 Create Date: 2021-03-29 18:38:24.512562
 
 """
+import importlib
+
 from alembic import op, context
 import sqlalchemy as sa
-import pkg_resources
-
-from gn_module_template import MODULE_CODE
 
 
 # revision identifiers, used by Alembic.
@@ -23,30 +22,10 @@ schema = 'gn_template'
 
 
 def upgrade():
-    op.execute("""
-        INSERT INTO gn_commons.t_modules (
-            module_code,
-            module_label,
-            module_path,
-            module_target,
-            module_picto,
-            active_frontend,
-            active_backend
-        ) VALUES (
-            'TEMPLATE',
-            'template',
-            'template',
-            '_self',
-            'fa-puzzle-piece',
-            TRUE,
-            TRUE
-        )
-    """)
-    operations = pkg_resources.resource_string("gn_module_template.migrations", f"data/schema.sql").decode('utf-8')
+    operations = importlib.resources.read_text("gn_module_template.migrations.data", "schema.sql")
     op.execute(operations)
 
 
 def downgrade():
     op.execute(f'DROP TABLE {schema}.my_table')
     op.execute(f'DROP SCHEMA {schema}')
-    op.execute(f"DELETE FROM gn_commons.t_modules WHERE module_code = '{MODULE_CODE}'")
